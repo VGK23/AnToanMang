d11d43a7bcef9a272d348a9ca293bc421eb05696
VGK <21522267@ms.uit.edu.vn>
2024-09-29 22:53:47 +0700

    Hoàn thành Caesar

---
+++ Caesar/Caesar/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Caesar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbPlaintext = new System.Windows.Forms.TextBox();
+            this.tbCiphertext = new System.Windows.Forms.TextBox();
+            this.btEncrypt = new System.Windows.Forms.Button();
+            this.btDecrypt = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbKey = new System.Windows.Forms.TextBox();
+            this.btDecryptNoKey = new System.Windows.Forms.Button();
+            this.btReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(217, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Plaintext";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(901, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ciphertext";
+            // 
+            // tbPlaintext
+            // 
+            this.tbPlaintext.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbPlaintext.Location = new System.Drawing.Point(56, 82);
+            this.tbPlaintext.Multiline = true;
+            this.tbPlaintext.Name = "tbPlaintext";
+            this.tbPlaintext.Size = new System.Drawing.Size(405, 451);
+            this.tbPlaintext.TabIndex = 2;
+            // 
+            // tbCiphertext
+            // 
+            this.tbCiphertext.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCiphertext.Location = new System.Drawing.Point(730, 82);
+            this.tbCiphertext.Multiline = true;
+            this.tbCiphertext.Name = "tbCiphertext";
+            this.tbCiphertext.Size = new System.Drawing.Size(405, 451);
+            this.tbCiphertext.TabIndex = 3;
+            // 
+            // btEncrypt
+            // 
+            this.btEncrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btEncrypt.Location = new System.Drawing.Point(508, 216);
+            this.btEncrypt.Name = "btEncrypt";
+            this.btEncrypt.Size = new System.Drawing.Size(179, 59);
+            this.btEncrypt.TabIndex = 4;
+            this.btEncrypt.Text = "Encrypt";
+            this.btEncrypt.UseVisualStyleBackColor = true;
+            this.btEncrypt.Click += new System.EventHandler(this.btEncrypt_Click);
+            // 
+            // btDecrypt
+            // 
+            this.btDecrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btDecrypt.Location = new System.Drawing.Point(508, 301);
+            this.btDecrypt.Name = "btDecrypt";
+            this.btDecrypt.Size = new System.Drawing.Size(179, 59);
+            this.btDecrypt.TabIndex = 5;
+            this.btDecrypt.Text = "Decrypt";
+            this.btDecrypt.UseVisualStyleBackColor = true;
+            this.btDecrypt.Click += new System.EventHandler(this.btDecrypt_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(579, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 22);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Key";
+            // 
+            // tbKey
+            // 
+            this.tbKey.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbKey.Location = new System.Drawing.Point(539, 148);
+            this.tbKey.Multiline = true;
+            this.tbKey.Name = "tbKey";
+            this.tbKey.Size = new System.Drawing.Size(121, 48);
+            this.tbKey.TabIndex = 7;
+            // 
+            // btDecryptNoKey
+            // 
+            this.btDecryptNoKey.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btDecryptNoKey.Location = new System.Drawing.Point(508, 386);
+            this.btDecryptNoKey.Name = "btDecryptNoKey";
+            this.btDecryptNoKey.Size = new System.Drawing.Size(179, 59);
+            this.btDecryptNoKey.TabIndex = 8;
+            this.btDecryptNoKey.Text = "Decrypt with no key";
+            this.btDecryptNoKey.UseVisualStyleBackColor = true;
+            this.btDecryptNoKey.Click += new System.EventHandler(this.btDecryptNoKey_Click);
+            // 
+            // btReset
+            // 
+            this.btReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btReset.Location = new System.Drawing.Point(508, 474);
+            this.btReset.Name = "btReset";
+            this.btReset.Size = new System.Drawing.Size(179, 59);
+            this.btReset.TabIndex = 9;
+            this.btReset.Text = "Reset";
+            this.btReset.UseVisualStyleBackColor = true;
+            this.btReset.Click += new System.EventHandler(this.btReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1197, 586);
+            this.Controls.Add(this.btReset);
+            this.Controls.Add(this.btDecryptNoKey);
+            this.Controls.Add(this.tbKey);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btDecrypt);
+            this.Controls.Add(this.btEncrypt);
+            this.Controls.Add(this.tbCiphertext);
+            this.Controls.Add(this.tbPlaintext);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Caesar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbPlaintext;
+        private System.Windows.Forms.TextBox tbCiphertext;
+        private System.Windows.Forms.Button btEncrypt;
+        private System.Windows.Forms.Button btDecrypt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbKey;
+        private System.Windows.Forms.Button btDecryptNoKey;
+        private System.Windows.Forms.Button btReset;
+    }
+}
+
---
+++ Caesar/Caesar/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Caesar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private char shiftChar(char c, int key)
+        {
+            if (Char.IsLetter(c)) //kiểm tra có phải là chữ cái hay không, nếu không sẽ bỏ qua 
+            {
+                char Base = Char.IsUpper(c) ? 'A' : 'a';  // Xác định ký tự gốc là chữ in hoa hay chữ thường
+                return (char)(((c - Base + key) % 26) + Base); // Thực hiện phép toán dịch chuyển trong bảng chữ cái
+            }
+            return c;
+        }
+
+        // Hàm xử lý chuỗi ký tự của plaintetx hoặc ciphertext
+        // Tham số `encrypt` quyết định xem có mã hóa hay không (true: mã hóa, false: giải mã)
+
+        // Const string& message: const sẽ đảm bảo messgae không thay đổi khi hàm được thực hiện
+        // đồng thời & sẽ thham chiếu tới đối tượng 
+        // mà không cần sao chép để tạo bản sao làm tốn dung lượng khi text nhập vào quá lớn
+        private string ProcessMessage(string message, int key, bool encrypt)
+        {
+            string result = "";
+            int effectiveKey = encrypt ? key : 26 - key; // Nếu là giải mã, key = 26 - key (theo quy tắc mã hóa Caesar)
+                                                         // Dịch từng ký tự của chuỗi
+            foreach (char c in  message)
+            {
+                result += shiftChar(c, effectiveKey);
+            }
+            return result;
+        }
+
+        private void btEncrypt_Click(object sender, EventArgs e)
+        {
+            int Key = int.Parse(tbKey.Text);
+            string Plaintext = tbPlaintext.Text.ToString();
+            tbCiphertext.Text = ProcessMessage(Plaintext, Key, true);
+        }
+
+        private void btDecrypt_Click(object sender, EventArgs e)
+        {
+            int Key = int.Parse(tbKey.Text);
+            string Ciphertext = tbCiphertext.Text.ToString();
+            tbPlaintext.Text = ProcessMessage(Ciphertext, Key, false);
+        }
+
+        private void btDecryptNoKey_Click(object sender, EventArgs e)
+        {
+            string Ciphertext = tbCiphertext.Text.ToString();
+            for (int i = 1; i <= 25; ++i)
+            {
+                tbPlaintext.Text += "Shift " + i + ": Decrypted message: " + ProcessMessage(Ciphertext, i, false) + "\r\n";
+            }
+        }
+
+        private void btReset_Click(object sender, EventArgs e)
+        {
+            tbPlaintext.Clear();
+            tbCiphertext.Clear();
+        }
+    }
+}
